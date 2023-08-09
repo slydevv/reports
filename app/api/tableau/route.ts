53647062c0a95f3fe16958804ae1bb37cad64048
silvertechhub <agubatasylvester@gmail.com>
2023-08-09 20:43:46 +0100

    first commit

--- app/api/tableau/route.ts
+++ app/api/tableau/route.ts
@@ -8,20 +8,22 @@ export function GET() {
   
   const response = NextResponse.next()
   // const uuid = uuidv4();
-   
+   const timenow = new Date().getTime();
+   const expiry = new Date().getTime() + 5 * 60 * 1000;
   
     const token = jwt.sign(
       {
         iss: process.env.TABLEAU_CLIENTID,
         sub: "support@eha.ng",
         aud: "tableau",
+        exp: expiry,
         jti: randomBytes(64).toString("hex"),
         scp: ["tableau:views:embed", "tableau:metrics:embed"]
       },
       process.env.TABLEAU_SECRETVALUE,
       {
         algorithm: "HS256",
-        expiresIn: 600,
+        // expiresIn: 600,
         header: {
           kid: process.env.TABLEAU_SECRETID,
           iss: process.env.TABLEAU_CLIENTID,
